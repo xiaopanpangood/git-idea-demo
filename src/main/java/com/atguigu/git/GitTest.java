6891b0141ede5386dea92b7009ce7a7dcb306c2e
xiaopan <1054386461@qq.com>
2022-08-30 21:12:04 +0800

    master添加新内容777

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("第一次添加");
         System.out.println("dev分支添加");
+        System.out.println("master添加新内容777");
     }
 }
